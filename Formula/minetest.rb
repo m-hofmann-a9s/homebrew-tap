4cbaa46a0b65ac7da40df3485a76c5246b8c4dfc
Martin Hofmann <mhofmann@anynines.com>
2024-12-10 13:14:22 +0100

    hardcode libpq.dylib

--- Formula/minetest.rb
+++ Formula/minetest.rb
@@ -76,11 +76,11 @@ class Minetest < Formula
       -DBUILD_SERVER=1
       -DENABLE_POSTGRESQL=1
       -DENABLE_FREETYPE=1
-      -DCMAKE_EXE_LINKER_FLAGS='-L#{Formula["freetype"].opt_lib},#{Formula["libpq"].opt_lib}'
+      -DCMAKE_EXE_LINKER_FLAGS='-L#{Formula["freetype"].opt_lib}'
       -DENABLE_GETTEXT=1
       -DCUSTOM_GETTEXT_PATH=#{Formula["gettext"].opt_prefix}
       -DPostgreSQL_INCLUDE_DIR='#{Formula["libpq"].opt_prefix}'
-      -DPostgreSQL_LIBRARY='#{Formula["libpq"].opt_lib}'
+      -DPostgreSQL_LIBRARY='#{Formula["libpq"].opt_lib}/libpq.dylib'
     ]
     # Workaround for 'Could NOT find GettextLib (missing: ICONV_LIBRARY)'
     args << "-DICONV_LIBRARY=#{MacOS.sdk_path}/usr/lib/libiconv.tbd" if OS.mac? && MacOS.version >= :big_sur
